3c2dd10597524432072fe6a086c8737aae876e59
ChantalMorales <chantal.morales@epn.edu.ec>
2021-03-10 20:08:49 -0500

    cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,5 +4,17 @@ import * as functions from 'firebase-functions';
 // // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
 // });
+
+
+exports.newDone = functions.firestore
+  .document('users/{userId}/done')
+  .onCreate((snap, context) => {
+    const newValue = snap.data();
+    // ejemplo de obtención de datos
+    const name = newValue.name;
+
+
+  });
